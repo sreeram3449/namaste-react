a81ce7e41559ff5a39deb3eaaf6a50ab23c3f47e
sreeram3449 <sreeram3449@gmail.com>
2024-02-09 19:06:47 +0530

    Episode 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
@@ -8,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am a H1 tag"),
+    React.createElement("h1", {}, "This is Namaste React"),
     React.createElement("h2", {}, "I am a H2 tag"),
   ]),
   React.createElement("div", { id: "child" }, [
---
+++ README.md
@@ -0,0 +1,20 @@
+# Namaste React 🚀
+
+
+# Parcel
+- Dev build
+- local server
+- HMR - Hot Module Replacement
+- File Watching algorithm - written in C++ 
+- Caching - Faster Builds
+- Image optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code splitting
+- Differential Bundling - to support older browsers
+- Diagnostics
+- Error Handling
+- HTTPS
+- Tree Shaking - removes unused code
